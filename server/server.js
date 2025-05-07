a516673249e265ccca038d59b73bac0ca2785055
Michael Daanoy <158813621+madaanoy@users.noreply.github.com>
2025-05-07 08:49:41 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://budget-allocation-bay.vercel.app/"
+    "https://budget-allocation-bay.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"], // Optional
 }));
