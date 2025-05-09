97b073848b6a2e29f76fe24f140cd47a81f1f169
Michael Daanoy <mdaanoy@gbox.adnu.edu.ph>
2025-05-09 09:08:05 +0800

    Server update

--- server/server.js
+++ server/server.js
@@ -9,12 +9,10 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://budget-allocation-bay.vercel.app",
-    "https://budget-allocation.onrender.com/" // In use frontend domain
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"], // Optional
+  origin: "https://budget-allocation.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 // Connect to MongoDB
