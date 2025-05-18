7c18b41c1dcb2a1e63bddd420d52ae39e3e5717a
EDToledana <edtoledana@gbox.adnu.edu.ph>
2025-05-18 22:01:39 +0800

    resolved useNavigate error in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import axios from "axios";
 import CreateBudget from "./CreateBudget";
 import BudgetDetails from "./BudgetDetails";
